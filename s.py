2c8f6931d1919511cee7ed4eafdbc8b7ae0213a9
xli2522 <50414011+xli2522@users.noreply.github.com>
2022-12-24 20:26:36 -0500

    add proper time and frequency domain signal downsampling options
    added downsample option to function sTransform.
    -> downsample = 'none' (default) or length (user input; target length in time)

--- s.py
+++ s.py
@@ -16,14 +16,17 @@
 import numpy as np
 import scipy.signal
 
-def sTransform(ts, sample_rate, frange=[0, 500], frate = 1, onesided=True, 
-                                                elevated=True, elevation=10e-8):
+def sTransform(ts, sample_rate, frange=[0, 500], frate = 1, downsample='none',
+                                onesided=True, elevated=True, elevation=10e-8):
     '''Compute the S Transform
     Input:
                 ts                 (ndarray)            time series data
-                sample_rate        (int)                sample rate
+                sample_rate        (int)                ts data sample rate
                 frange             (list, optional)     frequency range (Hz)
                 frate              (int, optional)      frequency sampling rate
+                downsample         (str, int, optional) down-sampled length
+                                                        in time ['none',
+                                                        length(int)]
                 onesided           (bool, optional)     include only the left 
                                                         side of the FFT if True
                 elevated           (bool, optional)     when True, add elevation
@@ -37,22 +40,59 @@ def sTransform(ts, sample_rate, frange=[0, 500], frate = 1, onesided=True,
                                                         time], lower -> higher
     '''
 
-    length = len(ts)               
+    length = len(ts)               # length of the input ts
     Nfreq = [int(frange[0]*length/sample_rate), 
-                        int(frange[1]*length/sample_rate)]     
-    tsVal = np.copy(ts)            
-    amp = np.zeros((int((Nfreq[1]-Nfreq[0])/frate)+1,length), dtype='c8')                    
-    tsFFT = np.fft.fft(tsVal)               
-    vec = np.hstack((tsFFT, tsFFT))         
+                        int(frange[1]*length/sample_rate)]
+    tsVal = np.copy(ts)            # copy of the input ts
+    number_freq = abs(int(Nfreq[1]-Nfreq[0]))   # total number of freq bins
+    _scaled_frate = int(np.ceil(frate*length/sample_rate)) # rescaled frate
+    #_scaled_frate = frate              # freq bin number
+    tsFFT = np.fft.fft(tsVal)                   # FFT of the original ts
 
-    # set the lowest frequency row to small values => 'zero' frequency     
-    amp[0] = np.fft.ifft(vec[0:length]*_window_normal(length, 0, 
-                                        elevated=elevated, elevation=elevation))             
-    for i in range(frate, (Nfreq[1]-Nfreq[0])+1, frate):                       
-        amp[int(i/frate)] = np.fft.ifft(
-                    vec[Nfreq[0]+i:Nfreq[0]+i+length]*_window_normal(
-                            length, Nfreq[0]+i, factor=1, elevated=elevated, 
-                                                        elevation=elevation))  
+    # time domain downsampling
+    if downsample=='none':
+        tsFFT_cut = np.concatenate((tsFFT, tsFFT))
+        downsampled_length = len(tsFFT)
+        normalize_cut = 1
+    elif isinstance(downsample, int):
+        tsFFT_positive = tsFFT[Nfreq[0]:Nfreq[1]]          # positive half
+        tsFFT_negative = tsFFT[-Nfreq[1]:len(tsFFT)
+                                            -Nfreq[0]]     # negative half
+        if downsample < 2*number_freq:
+            # perform the max allowed lower and higher frequency cut-off
+            # note: np.fft.fft gives array (of complex values) with of length: 
+            # 2*ts_length;
+            pass
+
+        else:
+            # 0 padding to make up for the high and low-passed freq elements
+            tsFFT_positive = np.concatenate((tsFFT_positive, 
+                        np.zeros(downsample//2-number_freq)))  
+            # 0 padding again
+            tsFFT_negative = np.concatenate((tsFFT_negative, 
+                        np.zeros(downsample//2-number_freq)))
+
+        # connect high and low-passed coefficients
+        tsFFT_cut = np.concatenate((tsFFT_positive, tsFFT_negative))
+        downsampled_length = len(tsFFT_cut)                 # new length 
+        # normalization coefficient
+        normalize_cut = 1 - downsampled_length/length
+
+    vec = np.hstack((tsFFT_cut, tsFFT_cut))      
+
+    # empty (0) spectrogram array
+    amp = np.zeros((int(number_freq/_scaled_frate)+1,downsampled_length), 
+                                                                dtype='c8')
+    # set the lowest frequency row to small values => 'zero' frequency 
+    # this step is solely for the purpose of 'recoverS()' function    
+    amp[0] = np.fft.ifft(vec[0:downsampled_length]
+                            *_window_normal(downsampled_length, 0, 
+                                elevated=elevated, elevation=elevation))
+    for i in range(_scaled_frate, number_freq+1, _scaled_frate):                       
+        amp[int(i/_scaled_frate)] = np.fft.ifft(
+                    vec[Nfreq[0]+i:Nfreq[0]+i+downsampled_length]
+                    *_window_normal(downsampled_length, Nfreq[0]+i, factor=1, 
+                        elevated=elevated, elevation=elevation)*normalize_cut)
     
     return amp
 
@@ -141,5 +181,5 @@ def inverseS(table, lowFreq = 0, elevated=True, elevation =10e-8):
         recovered_tsFFT[i] = np.fft.fft(tablep[i])[0]
 
     recovered_ts = np.fft.ifft(recovered_tsFFT)*2       
-    # assuming the input is real                                                    
+    # assuming the input is real
     return recovered_ts, recovered_tsFFT
--- test.py
+++ test.py
@@ -21,44 +21,66 @@ from s import *
 def test():
 
     # Generate a quadratic chirp signal
-    dt = 0.0001
+    dt = 0.0001; t = 3
     rate = int(1/dt)
-    ts = np.linspace(0, 1, int(1/dt))
+    ts = np.linspace(0, t, int(t/dt))
     
-    data = scipy.signal.chirp(ts, 10, 1, 120, method='quadratic')
+    data = scipy.signal.chirp(ts, 10, t, 120, method='quadratic')
 
     # Compute S Transform Spectrogram
-    spectrogram = sTransform(data, sample_rate=rate, frange=[0,500])
+    spectrogram = sTransform(data, sample_rate=rate, frate=rate/len(data),
+                                        downsample='none', frange=[0,500])
     plt.imshow(abs(spectrogram), origin='lower', aspect='auto')
     plt.title('Original Spectrogram')
     plt.show()
 
     # Quick Recovery of ts from S Transform 0 frequency row
     recovered_ts = recoverS(spectrogram)
-    plt.plot(recovered_ts-data)
-    plt.title('Time Series Reconstruction Error')
+    fig, axs = plt.subplots(2,1)
+    axs[0].plot(data)
+    axs[1].plot(recovered_ts.real)
+    axs[0].set_title('Original Signal')
+    axs[1].set_title('(recoverS) Freq-passed, down-sampled Signal')
     plt.show()
+    # plt.plot(recovered_ts-data)
+    # plt.title('Time Series Reconstruction Error')
+    # plt.show()
 
     # Compute S Transform Spectrogram on the recovered time series
+    # IMPORTANT: note that now the sample_rate might have changed
+    # new sample_rate is =len(recovered_ts)/len(data)*rate;
+    # in this example we used downsampl='none' so new sample_rate = sample_rate
     recoveredSpectrogram = sTransform(recovered_ts, 
-                                        sample_rate=rate, frange=[0,500])
+                        sample_rate=len(recovered_ts)/len(data)*rate, 
+                                                        frange=[0,500])
     plt.imshow(abs(recoveredSpectrogram), origin='lower', aspect='auto')
-    plt.title('Recovered Specctrogram')
+    plt.title('(recoverS) Recovered Specctrogram')
     plt.show()
 
     # Quick Inverse of ts from S Transform
     inverse_ts, inverse_tsFFT = inverseS(spectrogram)
-    plt.plot(inverse_ts)
-    plt.plot(inverse_ts-data)
-    plt.title('Time Series Reconstruction Error')
-    plt.legend(['Recovered ts', 'Error'])
+    fig, axs = plt.subplots(2,1)
+    axs[0].plot(data)
+    axs[1].plot(inverse_ts.real)
+    axs[0].set_title('Original Signal')
+    axs[1].set_title('(inverseS) Freq-passed, down-sampled Signal')
     plt.show()
 
+    # plt.plot(inverse_ts)
+    # plt.plot(inverse_ts-data)
+    # plt.title('Time Series Reconstruction Error')
+    # plt.legend(['Recovered ts', 'Error'])
+    # plt.show()
+
     # Compute S Transform Spectrogram on the recovered time series
+    # IMPORTANT: the inverse transform itself is exact;
+    # however, information could be lost in the forward ST due to downsampling
+    # in both time and frequency
     inverseSpectrogram = sTransform(inverse_ts, 
-                                        sample_rate=rate, frange=[0,500])
+                            sample_rate=len(recovered_ts)/len(data)*rate, 
+                                                        frange=[0,500])
     plt.imshow(abs(inverseSpectrogram), origin='lower', aspect='auto')
-    plt.title('Recovered Specctrogram')
+    plt.title('Recovered Specctrogram (inverseS)')
     plt.show()
 
     return 
